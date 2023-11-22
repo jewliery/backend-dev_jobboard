aaef453b1ec7eea96a4c0805f85062f801f62eb8
Jules <julienne.buechele@gmail.com>
2023-11-22 20:04:20 +0100

    Delete old comments

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,23 +15,19 @@ class JobsController < ApplicationController
     end
 
     def edit
-        # @job = Job.find(params[:id])
     end
 
     def update
-        # @job = Job.find(params[:id])
         @job.update(job_params)
         redirect_to jobs_path
     end
 
     def destroy
-        # @job = Job.find(params[:id])
         @job.destroy
         redirect_to jobs_path
     end
 
     def show
-        # @job = Job.find(params[:id])
     end
     
 
