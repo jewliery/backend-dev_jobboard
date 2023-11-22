00788bb0e18f9302342324defe545d75d770aeb9
Jules <julienne.buechele@gmail.com>
2023-11-22 20:00:32 +0100

    Refactor job_controller and adjsut edit view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+    before_action :set_job, only: [:show, :edit, :update, :destroy]
+
     def index
         @jobs = Job.all
     end
@@ -13,23 +15,23 @@ class JobsController < ApplicationController
     end
 
     def edit
-        @job = Job.find(params[:id])
+        # @job = Job.find(params[:id])
     end
 
     def update
-        @job = Job.find(params[:id])
+        # @job = Job.find(params[:id])
         @job.update(job_params)
         redirect_to jobs_path
     end
 
     def destroy
-        @job = Job.find(params[:id])
+        # @job = Job.find(params[:id])
         @job.destroy
         redirect_to jobs_path
     end
 
     def show
-        @job = Job.find(params[:id])
+        # @job = Job.find(params[:id])
     end
     
 
@@ -37,5 +39,9 @@ class JobsController < ApplicationController
 
     def job_params
         params.require(:job).permit(:title, :description, :long_description)
+    end
+
+    def set_job
+        @job = Job.find(params[:id])
     end
 end
